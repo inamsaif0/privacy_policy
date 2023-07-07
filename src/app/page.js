c323c3088fc09c7c1f3f2a90e54e6c72b0773648
inamsaif0 <inamsaif586@gmail.com>
2023-07-07 03:08:32 -0700

    data

---
+++ src/app/page.js
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+
+      <p>Effective Date: 7/7/2023</p>
+
+      <h2>1. Information We Collect</h2>
+
+      <h3>1.1 Personal Information:</h3>
+      <p>When you create an account with OTPeng, we collect your name, email address, and phone number to provide our services and authenticate your account.</p>
+
+      <h3>1.2 Device Information:</h3>
+      <p>We may collect information about the device you use to access OTPeng, including device type, operating system, and unique device identifiers. This helps us optimize our application for different devices.</p>
+
+      <h3>1.3 Location Information:</h3>
+      <p>With your consent, we may collect your device's location information to provide location-based features, such as finding nearby OTPeng service providers.</p>
+
+      <h2>2. Use of Information</h2>
+
+      <h3>2.1 Providing Services:</h3>
+      <p>We use the collected information to provide our services to you, including sending one-time passwords (OTPs) for authentication and facilitating communication between users.</p>
+
+      <h3>2.2 Improving User Experience:</h3>
+      <p>We may use anonymized and aggregated data for analytics purposes to understand user behavior and improve our application's functionality and user experience.</p>
+
+      <h3>2.3 Communication:</h3>
+      <p>We may use your email address or phone number to send important updates, notifications, or promotional offers related to OTPeng. You can opt out of receiving promotional communications at any time.</p>
+
+      <h2>3. Data Sharing</h2>
+
+      <h3>3.1 Third-Party Service Providers:</h3>
+      <p>We may share your information with trusted third-party service providers who assist us in delivering our services, such as cloud hosting providers or SMS gateway providers. These providers are obligated to protect your information and are not permitted to use it for any other purpose.</p>
+
+      <h3>3.2 Legal Compliance:</h3>
+      <p>We may disclose your information if required by law, court order, or governmental regulation, or to protect our rights, property, or safety, or the rights, property, or safety of others.</p>
+
+      <h2>4. Data Security</h2>
+
+      <p>We implement industry-standard security measures to protect your information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is completely secure. Therefore, we cannot guarantee absolute security of your information.</p>
+
+      <h2>5. User Rights</h2>
+
+      <p>You have the right to access, update, correct, or delete the personal information we hold about you. You can do this by accessing your account settings within the OTPeng application or by contacting our support team.</p>
+
+      <h2>6. Consent and Opt-Out</h2>
+
+      <p>By using OTPeng, you consent to the collection and use of your information as outlined in this privacy policy. You can withdraw your consent or opt out of certain data collection practices by deleting your account or contacting our support team.</p>
+
+      <h2>7. Changes to the Privacy Policy</h2>
+
+      <p>We may update this privacy policy from time to time. We will notify you of any material changes by posting the updated policy within the OTPeng application or by email.</p>
+    </main>
+  )
+}
